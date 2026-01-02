73d4e7c25b67cf78e88b85fa21c42a839f02640f
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-02 17:25:29 +0100

    Used BookSelectionService to share the selected book across view models.

---
+++ BookStore_Presentation/Services/BookSelectionService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStore_Domain;
+
+namespace BookStore_Presentation.Services
+{
+    public class BookSelectionService
+    {
+        public BookAdminItem? SelectedBook { get; set; }
+    }
+}
