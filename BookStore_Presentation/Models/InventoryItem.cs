327f773095de00b0e39c10146d68a2e580449343
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-06 11:42:47 +0100

    Delete INotifyPropertyChanged and implemented  INotifyPropertyChanged from ViewModelBase.cs instead

---
+++ BookStore_Presentation/Models/InventoryItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using BookStore_Presentation.ViewModels;
+
+
+
+
+namespace BookStore_Presentation.Models
+{
+    public class InventoryItem : ViewModelBase
+    {
+        private int _quantity;
+        public string ISBN { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int StoreId { get; set; }
+        public int Quantity
+        {
+            get => _quantity;
+            set
+            {
+                if (_quantity != value)
+                {
+                    _quantity = value;
+                    RaisePropertyChanged();
+                }
+            }
+
+        }
+
+    }
+}
+    
