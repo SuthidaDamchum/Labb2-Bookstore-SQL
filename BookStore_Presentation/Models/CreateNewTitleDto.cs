312c2531664472e25eaeb01a3ce3727f7c0a2615
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-06 19:17:10 +0100

    Created CreateNewTitleDto to prevent direct UI-to-data access

---
+++ BookStore_Presentation/Models/CreateNewTitleDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore_Presentation.Models
+{
+    internal class CreateNewTitleDto
+    {
+        public string Isbn13 { get; set; } = null!;
+        public string Title { get; set; } = null!;
+
+        public string? Language { get; set; }
+
+        public decimal? Price { get; set; }
+
+        public DateOnly? PublicationDate { get; set; }
+
+        public int? PageCount { get; set; }
+
+        public int? GenreId { get; set; }
+
+        public int? PublisherId { get; set; }
+
+        public List<int> AuthorIds { get; set; } = new List<int>();
+
+    }
+}
+
