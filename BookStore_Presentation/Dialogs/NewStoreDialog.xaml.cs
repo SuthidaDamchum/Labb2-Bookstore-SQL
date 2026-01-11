adffb7335d3566014478492aec8f4338414f13a0
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-01-11 18:43:20 +0100

    -Added dialog for creating new store

---
+++ BookStore_Presentation/Dialogs/NewStoreDialog.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BookStore_Presentation.ViewModels;
+
+namespace BookStore_Presentation.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for NewStore.xaml
+    /// </summary>
+    public partial class NewStoreDialog : Window
+    {
+        public NewStoreDialog()
+        {
+            InitializeComponent();
+            DataContext = new NewStoreViewModel();
+        }
+    }
+}
